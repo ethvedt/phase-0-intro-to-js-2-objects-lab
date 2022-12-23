f83f6d416f9a60793949c3a3899499b0dbad4b36
Eric Thvedt <ericsthvedt@gmail.com>
2022-12-22 18:00:53 -0800

    Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {"name" : "default",
+    "streetAddress" : "default"}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const employee = {...obj}
+    employee[key]= value
+    return employee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const employee = {...obj}
+    delete employee[key]
+    return employee
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj
+}
